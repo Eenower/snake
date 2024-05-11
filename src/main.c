6352fcfb6b7835a02ecdec6dba9ec2567ce37414
Eenower <Eenower@protonmail.com>
2024-05-11 20:54:03 +1000

    screen displays and added game loop, i should have been doing school

--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@
 int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+int last_frame_time = 0;
+
+struct ball {
+    float x;
+    float y;
+    float width;
+    float height;
+} ball;
 
 int initialize_window(void){
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
@@ -49,14 +57,42 @@ void process_input(){
     }
 }
 void update(){
-    //TODO
+    // waste time until we reach frame target time
+  //  int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
+    
+  //  if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+//	SDL_Delay(time_to_wait);
+//    }
+    // get delta time factor that will be used to update objects later
+    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
+    ball.x += 70 * delta_time;
 }
 
 void render(){
-    //TODO
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // draw game objects
+
+    SDL_Rect ball_rect = { 
+	(int)ball.x,
+	(int)ball.y,
+	(int)ball.width,
+	(int)ball.height
+    };
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &ball_rect);
+    
+    SDL_RenderPresent(renderer);
+    last_frame_time = SDL_GetTicks();
+
 }
 void setup(){
-    //TODO
+    
+     ball.x = 20;
+     ball.y = 20;
+     ball.width = 15;
+     ball.height = 15;
 }
 
 void destroy_window(){
@@ -71,7 +107,7 @@ int main() {
     while (game_is_running) {
 	process_input();
 	update();
-	render();
+        render();
     }
     destroy_window();
 
